6bea3c9ff709c9ace04e70b80d756dd7f687e21a
Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-09-05 23:09:53 +0300

    feat: database update

---
+++ back/routes/ExpenseRoutes.js
@@ -0,0 +1,19 @@
+app.get("/api/expenses", (req, res) => {
+
+});
+
+app.post("/api/expenses", (req, res) => {
+
+});
+
+app.get("/api/expense/:id", (req, res) => {
+
+});
+
+app.put("/api/expenses/:id", (req, res) => {
+
+});
+
+app.delete("/api/expenses/:id", (req, res) => {
+
+});
