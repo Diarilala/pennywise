c2e38482ce8e97feb94a59e178aab10a7651bbeb
Lavorary <hei.lavorary@gmail.com>
2025-09-07 16:54:08 +0300

    feat : first version of categoryController(get categories)

--- back/Controller/CategoryController.js
+++ back/Controller/CategoryController.js
@@ -1,14 +1,29 @@
-import { PrismaClient } from "@prisma/client";
-import express from 'express';
-
+const { PrismaClient } = require('@prisma/client');
+const express = require("express");
 const prisma = new PrismaClient();
+
 const app = express();
+const PORT = process.env.PORT ;
 
+app.get("/api/categories", async (req, res) => {
+    try {
+        const { user_id } = req.query;
 
-app.get("/api/categories", (req, res) => {
+        if (!user_id) {
+            return res.status(400).json({ error: 'Sorry, user ID is required' });
+        }
 
+        const categories = await prisma.categories.findMany({
+            where: {
+                user_id: user_id
+            },
+
+        });
+
+        res.json(categories);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Oh no, something went wrong' });
+    }
 });
 
-app.post("/api/categories", (req, res) => {
-
-});
