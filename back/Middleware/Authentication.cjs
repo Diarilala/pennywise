7b99868bc94d1a487708963e09dc7691663c9faf
Diarilala <hei.diarilala@gmail.com>
2025-09-07 22:18:10 +0300

    feat: add middleware for protected routes

--- back/Middleware/Authentication.cjs
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const dotenv = require('dotenv');
-dotenv.config();
-process.env.TOKEN_SECRET;
-
-export function generateAccessToken(username) {
-    return jwt.sign({ username }, process.env.TOKEN_SECRET, {expiresIn:'86400s'})
-}
---
+++ back/Middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const middleware = (req, res, next) => {
+    try {
+        const token = req.cookies?.token;
+
+        if (!token) {
+            return res.status(401).json({
+                message: 'No token provided',
+            });
+        }
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        if (error.name === "TokenExpiredError") {
+            return res.status(403).json({
+                message: 'Token expired',
+            })
+        } else if (error.name === "JsonWebTokenError") {
+            return res.status(403).json({
+                message: 'Invalid token',
+            })
+        }
+        return res.status(500).json({
+            message: 'Internal Server Error',
+        })
+    }
+};
