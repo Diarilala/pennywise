e7fbecdd8295d1f9c5252872883fb90e975c3e5e
Diarilala <hei.diarilala@gmail.com>
2025-09-08 15:40:29 +0300

    feat: add delete function

--- back/Controller/CategoryController.js
+++ back/Controller/CategoryController.js
@@ -1,20 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 import req from "express/lib/request.js";
 import res from "express/lib/response.js";
+import middleware from "../Middleware/authMiddleware.js";
 
 const prisma = new PrismaClient();
 
-export async function updateCategory(categoryId, userId, updatedCategory) {
+export async function updateCategory(categoryId, updatedCategory) {
     try {
-        const Category = await prisma.categories.findUnique(
+        const category = await prisma.categories.findUnique(
             {
                 where: {
                     category_id: categoryId,
-                    user_id: req.user.user_id
+                    user_id: middleware(req.user.user_id)
                 }
             }
         );
-        if (!Category) {
+        if (!category) {
             console.error("Category not found");
             res.status(404).json({
                 message: 'Category not found'
@@ -33,6 +34,42 @@ export async function updateCategory(categoryId, userId, updatedCategory) {
             }
         );
     }catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: 'Internal Server Error',
+        })
+    }
+}
+
+export async function deleteCategory(categoryId) {
+    try {
+        const category = await prisma.categories.findUnique(
+            {
+                where: {
+                    category_id: categoryId,
+                    user_id: middleware(req.user.user_id)
+                }
+            }
+        );
+        if (!category) {
+            console.error("Category not found");
+            res.status(404).json({
+                message: 'Category not found'
+            })
+        }
+
+        const deleteCategory = await prisma.categories.delete(
+            {
+                where: {
+                    category_id: categoryId,
+                    user_id: middleware(req.user.user_id)
+                }
+            }
+        )
+        res.status(200).json({
+            message: 'Category deleted successfully'
+        })
+    }   catch (error) {
         console.error(error);
         res.status(500).json({
             error: 'Internal Server Error',
