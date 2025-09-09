ddc3e62a91b41d308605f79a93d58b2a30d8a967
nyainanathan <nyaina.nathan.1@gmail.com>
2025-09-09 07:00:27 +0300

    feat: api call working

--- back/index.js
+++ back/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import expenseRoutes from './routes/ExpenseRoutes.js'
 import receiptRoutes from './routes/ReceiptRoutes.js'
-
+import cors from 'cors'
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
-app.use('/expense', expenseRoutes )
+app.use('/api/expense', expenseRoutes )
 
 app.use('/receipt', receiptRoutes);
 
---
+++ front/src/Components/ExpenseSection.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import Expense from "./Expense";
+
+const ExpenseSection = () => {
+
+    const [ expenses, setExpenses ] = useState([]);
+
+    useEffect(() => {
+        const fetchExpenses = async () => {
+            let token ='550e8400-e29b-41d4-a716-446655440000';
+            token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNTUwZTg0MDAtZTI5Yi00MWQ0LWE3MTYtNDQ2NjU1NDQwMDAwIiwiaWF0IjoxNzU3MzY2NTQ1fQ.XBG6M9ge7k5Py87pJ3i7bEbkaNLI5N2-fBdGpXFubdo'
+
+            // localStorage.getItem('jwt');
+            const thisUserExpenses = await fetch('http://localhost:3000/api/expense', {
+                method: 'GET',
+                headers : {
+                    'Authorization' : `Bearer ${token}`
+                }
+            } )
+            const data = await thisUserExpenses.json();
+            setExpenses(data);
+            console.log(data);
+            
+        }
+        fetchExpenses();
+    }, [expenses]);
+
+    return (
+        <>
+            {expenses.map((el, index) => (
+                <li key={index}>{el}</li>
+            ))}
+        </>
+    )
+
+}
+export default ExpenseSection;
