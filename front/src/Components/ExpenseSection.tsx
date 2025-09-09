fc4613d53c983f12eccea2726d1f8a9a8d304330
nyainanathan <nyaina.nathan.1@gmail.com>
2025-09-09 08:07:57 +0300

    feat: expense interface added to the expense section component

--- front/src/Components/ExpenseSection.tsx
+++ front/src/Components/ExpenseSection.tsx
@@ -1,9 +1,20 @@
 import { useEffect, useState } from "react";
 import Expense from "./Expense";
 
+interface Expense {
+    expense_id : string,
+    user_id : string,
+    category_id: string,
+    amount : number,
+    type : string,
+    date : string,
+    start_date : string,
+    end_date : string,
+    description : string
+}
 const ExpenseSection = () => {
 
-    const [ expenses, setExpenses ] = useState([]);
+    const [ expenses, setExpenses ] = useState<Expense[]>([]);
 
     useEffect(() => {
         const fetchExpenses = async () => {
